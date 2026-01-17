508175a6ffc402390c8ecdb59f44eeb6e13c7add
Alan Ugarte <alan.ugarte.pe@gmail.com>
2026-01-17 11:21:02 -0500

    Se crean los modelos para user y task

---
+++ src/tasks/task.interface.ts
@@ -0,0 +1,16 @@
+export type TaskStatus = 'pending' | 'in_progress' | 'completed';
+export type TaskPriority = 'low' | 'medium' | 'high';
+
+
+export interface Task {
+    id: string;
+    title: string;
+    description: string;
+    status: TaskStatus;
+    priority: TaskPriority;
+    assignedTo: string | null;
+    createdBy: string;
+    dueDate: Date;
+    createdAt: Date;
+    updatedAt: Date;
+}

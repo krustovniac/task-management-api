109db1a2dcfafaeb5d99aa972a2da021ee4d41f2
Alan Ugarte <alan.ugarte.pe@gmail.com>
2026-01-17 12:21:50 -0500

    Lógica del ejercicio(negocio) y puntos de entrada de users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [];
+
+    create(dto: CreateUserDto): User {
+        if (this.users.some(u => u.email === dto.email)) {
+            throw new BadRequestException('Email ya existe');
+    }
+
+    const user: User = {
+        id: randomUUID(),
+        ...dto,
+        createdAt: new Date(),
+    };
+
+    this.users.push(user);
+        return user;
+    }
+
+    findAll(): User[] {
+        return this.users;
+    }
+
+    findOne(id: string): User {
+        const user = this.users.find(u => u.id === id);
+        if (!user) throw new NotFoundException('Usuario no encontrado');
+        return user;
+    }
+}
